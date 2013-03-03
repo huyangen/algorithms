d2fb46660875303560cc168776c2e5ffd6903cc8
huyangen <huyangen@qq.com>
2013-03-03 10:36:06 +0800

    test

---
+++ src/test.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @author Administrator
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a={9,8,7,6,5,1,10,4,3,2,0,1000000};
+		//HeapSort.heapSort(a);
+		//HeapSort.display(a);
+		//QuickSort.display(a);
+		//QuickSort.quickSort(a);
+		InsertSort.insertSort(a);
+		QuickSort.display(a);
+	}
+
+}
